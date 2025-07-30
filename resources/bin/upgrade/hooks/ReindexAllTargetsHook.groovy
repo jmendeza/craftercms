7ef37ccaa431dd8a93d74a35028c08f26e912b36
Jonathan Mendez <jonnathan.mendez@gmail.com>
2025-07-30 11:49:32 -0600

    Upgrade okhttp

--- resources/bin/upgrade/hooks/ReindexAllTargetsHook.groovy
+++ resources/bin/upgrade/hooks/ReindexAllTargetsHook.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2023 Crafter Software Corporation. All Rights Reserved.
+ * Copyright (C) 2007-2025 Crafter Software Corporation. All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as published by
@@ -16,7 +16,7 @@
 package upgrade.hooks
 
 @Grapes([
-	@Grab(group = 'com.squareup.okhttp3', module = 'okhttp', version = '4.12.0')
+		@Grab(group = 'com.squareup.okhttp3', module = 'okhttp-jvm', version = '5.1.0')
 ])
 
 import groovy.json.JsonSlurper
