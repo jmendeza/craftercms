523643251c0ad86a7a8d7fb79a8efad6d55f4a27
Jonathan Mendez <jonnathan.mendez@gmail.com>
2026-08-13 11:27:47 -0600

    Add cleanup support to UM for 3.x Index group_ibfk_1

--- studio/src/main/resources/crafter/studio/database/upgrade/4.5.x/4.5.1.0-to-4.5.1.1.sql
+++ studio/src/main/resources/crafter/studio/database/upgrade/4.5.x/4.5.1.0-to-4.5.1.1.sql
@@ -17,6 +17,8 @@
 ALTER TABLE `audit` DROP COLUMN IF EXISTS `organization_id` ;
 
 ALTER TABLE `group`
+	DROP FOREIGN KEY IF EXISTS `group_ibfk_1`,
+	DROP INDEX IF EXISTS `group_ibfk_1`,
 	DROP FOREIGN KEY IF EXISTS `group_ix_org_id`,
 	DROP INDEX IF EXISTS `group_ix_org_id`,
 	DROP COLUMN IF EXISTS `org_id` ;
