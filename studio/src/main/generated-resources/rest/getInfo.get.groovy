d279645d5825ab417e09baa7252ffd159def9a6b
Jonathan Méndez <jonnathan.mendez@gmail.com>
2026-08-18 14:50:41 -0600

    Fix build number replacement in version info template (#8928)

--- studio/src/main/generated-resources/rest/getInfo.get.groovy
+++ studio/src/main/generated-resources/rest/getInfo.get.groovy
@@ -15,8 +15,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-return ["version":"${project.version}",
-         "id":"${buildNumber}",
-          "uiId":"${UIbuildNumber}",
-          "buildDate":new Date(${timestamp})
+return ["version":"@version@",
+         "id":"@buildNumber@",
+          "uiId":"@UIbuildNumber@",
+          "buildDate":new Date(@timestamp@)
 ]
